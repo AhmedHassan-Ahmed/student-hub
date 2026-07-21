a6b83f936e06a3f67e540537aed1157cb5ed3302
AhmedHassan-Ahmed <ahmedhassan8754321@gmail.com>
2026-07-21 23:52:11 +0300

    fix tasks page

--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -2,24 +2,32 @@ import Button from "../components/Button";
 import { Download } from "lucide-react";
 import TaskPopup from "../components/TaskPopup";
 import TasksCard from "../components/TasksCard";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import exportCSV from "../functions/exportCSV";
-import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import PageMainHeader from "../components/PageMainHeader";
+
 export default function Tasks() {
   const dialogRef = useRef();
+
   const tasks = JSON.parse(localStorage.getItem("Tasks")) || [];
   const [updatetasks, settasks] = useState(tasks);
 
+  const open = () => dialogRef.current.showModal();
+
+  const close = () => dialogRef.current.close();
+
   const handleDelete = (id) => {
-    const updatedTasks = tasks.filter((task) => task.id !== id);
+    const updatedTasks = updatetasks.filter((task) => task.id !== id);
 
     settasks(updatedTasks);
+
     localStorage.setItem("Tasks", JSON.stringify(updatedTasks));
   };
+
   const handleSubmit = (e) => {
     e.preventDefault();
+
     const formData = new FormData(e.target);
 
     const task = [
@@ -34,17 +42,15 @@ export default function Tasks() {
       },
     ];
 
-    const updated = tasks;
-    updated.push(...task);
-    const finaltasks = JSON.stringify(updated);
+    const updated = [...updatetasks, ...task];
+
     settasks(updated);
-    localStorage.setItem("Tasks", finaltasks);
+
+    localStorage.setItem("Tasks", JSON.stringify(updated));
+
     close();
     e.target.reset();
   };
-
-  const open = () => dialogRef.current.showModal();
-  const close = () => dialogRef.current.close();
 
   return (
     <>
@@ -66,7 +72,10 @@ export default function Tasks() {
         />
       </PageMainHeader>
 
-      <TasksCard handleDelete={handleDelete} tasksupdate={updatetasks || []} />
+      <TasksCard
+        handleDelete={handleDelete}
+        tasksupdate={updatetasks}
+      />
     </>
   );
 }
