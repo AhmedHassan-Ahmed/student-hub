831edc1570a73baa522b77a5286e563c5e7eef91
Ghofran Mohamed Saad <mghofran88@gmail.com>
2026-07-25 00:52:38 +0300

    remove landing nav & edit dark and light botton place

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -12,7 +12,7 @@ import ThemeButton from "./ThemeButton";
 
 const Navbar = ({ open, setOpen }) => {
   const linkClass = ({ isActive }) =>
-    `flex items-center gap-2 rounded-lg px-4 py-2 text-sm font-medium transition-all duration-200 ${
+    `flex items-center gap-2 rounded-lg px-4 py-2 text-l font-medium transition-all duration-200 ${
       isActive
         ? "bg-blue-100 text-blue-700"
         : "text-gray-600 hover:bg-gray-100 hover:text-blue-600"
@@ -28,16 +28,16 @@ const Navbar = ({ open, setOpen }) => {
       <div className="mx-auto flex h-16 max-w-[114rem] items-center justify-between px-6">
         <NavLink
           to="/"
-          className="text-xl font-bold tracking-tight text-blue-700"
+          className="text-3xl font-bold tracking-tight text-blue-700"
         >
           Student-Hub
         </NavLink>
 
-        <div className="flex items-center gap-2">
-          <ThemeButton />
-        </div>
-
         <div className="hidden items-center gap-2 md:flex">
+          <div className="flex items-center gap-2">
+            <ThemeButton />
+          </div>
+
           <NavLink to="/" className={linkClass} end>
             <FaHome />
             Home
